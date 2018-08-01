dcda700c6f5b98b4e52b5c7b2917f895d93d0775
iruhary <iruhary@gmail.com>
2018-08-01 12:47:28 -0500

    Actualizacionn

---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Models/MediaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoOnDemand.Web.Models
+{
+    public class MediaViewModel
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int? duracionMin { get; set; }
+        public DateTime? fechaRegistro { get; set; }
+        public DateTime? fechaLanzamiento { get; set; }
+        public EEstatusMedia? estado { get; set; }
+    }
+}
---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Models/OpinionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoOnDemand.Web.Models
+{
+    public class OpinionViewModel
+    {
+        public int Id { get; set; }
+        public int? Puntuacion { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public int? UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+        public int? MediaId { get; set; }
+        public Media Media { get; set; }
+    }
+}
