33d39af7c2219378f7cb9932aeba17074749c6d4
JIgnacioTG <travag97@gmail.com>
2018-08-01 13:50:36 -0500

    Last Changes for Today

---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Mapping/OpinionMap.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using VideoOnDemand.Entities;
+
+namespace VideoOnDemand.Mapping
+{
+    class OpinionMap : EntityTypeConfiguration<Opinion>
+    {
+
+        public OpinionMap()
+        {
+
+            ToTable("Opinion");
+
+            HasKey(o => o.Id);
+            Property(o => o.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(o => o.Puntuacion)
+                .IsOptional();
+            Property(o => o.Descripcion)
+                .IsOptional();
+        }
+
+    }
+}
