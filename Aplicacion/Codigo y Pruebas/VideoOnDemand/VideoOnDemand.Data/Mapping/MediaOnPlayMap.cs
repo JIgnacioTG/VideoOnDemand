eb09b72384d80f7282353cea7128ae6846cf89cb
JIgnacioTG <travag97@gmail.com>
2018-08-01 13:03:19 -0500

    More mappings

---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Mapping/MediaOnPlayMap.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using VideoOnDemand.Entities;
+
+namespace VideoOnDemand.Mapping
+{
+    class MediaOnPlayMap : EntityTypeConfiguration<MediaOnPlay>
+    {
+
+        public MediaOnPlayMap()
+        {
+
+            ToTable("MediaOnPlay");
+            HasKey(m => m.Id);
+            Property(m => m.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(m => m.Milisegundo)
+                .IsOptional();
+
+            HasRequired(m => m.Media)
+                .WithMany()
+                .HasForeignKey(m => m.MediaId);
+
+            HasRequired(m => m.Usuario)
+                .WithMany()
+                .HasForeignKey(m => m.UsuarioId);
+
+        }
+
+    }
+}
