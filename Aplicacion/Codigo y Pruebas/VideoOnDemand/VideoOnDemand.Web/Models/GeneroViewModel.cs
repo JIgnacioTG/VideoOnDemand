2904080b026f3fe06d2899c4431f0434904838db
iruhary <iruhary@gmail.com>
2018-08-01 11:50:11 -0500

    Agregar vistas

---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Models/GeneroViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VideoOnDemand.Web.Models
+{
+    public class GeneroViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        [DisplayName("nombre del género")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        [DisplayName("descripción del género")]
+        public string Descripcion { get; set; }
+    }
+}
