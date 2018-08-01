0f3b95c62e2aa72b95cab9ad71371dd2f91cd920
EEstradaAcosta <33323957+EEstradaAcosta@users.noreply.github.com>
2018-08-01 11:24:54 -0500

    Add Entities <Movie> <Serie> <Episodio>

---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Entities/Episodio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoOnDemand.Entities
+{
+    public class Episodio : Media
+    {
+        public int temporada { get; set; }
+        public int serieId { get; set; }
+    }
+}
