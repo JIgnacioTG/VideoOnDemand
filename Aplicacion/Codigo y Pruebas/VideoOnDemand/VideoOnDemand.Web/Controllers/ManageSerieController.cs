5fa5ed1fc6a97a1a86935035e8ab9da93cfa1969
JIgnacioTG <travag97@gmail.com>
2018-08-02 11:55:47 -0500

    Create on ManageSerie

--- Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Controllers/ManageSerieController.cs
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Controllers/ManageSerieController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VideoOnDemand.Data;
+using VideoOnDemand.Repositories;
+using VideoOnDemand.Web.Helpers;
+using VideoOnDemand.Web.Models;
 
 namespace VideoOnDemand.Web.Controllers
 {
-    public class ManageSerieController : Controller
+    public class ManageSerieController : BaseController
     {
         // GET: ManageSerie
         public ActionResult Index()
         {
-            return View();
+
+            VideoOnDemandContext context = new VideoOnDemandContext();
+            SerieRepository repository = new SerieRepository(context);
+
+            // Consultar las series
+            var lst = repository.GetAll();
+
+            // Mapear la lista de series con una lista de SerieViewModel
+            var models = MapHelper.Map<IEnumerable<SerieViewModel>>(lst);
+
+            return View(models);
         }
 
         // GET: ManageSerie/Details/5
