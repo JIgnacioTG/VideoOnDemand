22f703de6ec8691348d14579c1a201f8357199c2
JIgnacioTG <travag97@gmail.com>
2018-08-02 09:28:54 -0500

    Fixing namespace and using
    Adding more repositories

--- Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Repositories/GeneroRepository.cs
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Repositories/GeneroRepository.cs
@@ -1,10 +1,4 @@
-﻿using ltracker.Data.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VideoOnDemand.Data;
+﻿using VideoOnDemand.Data;
 using VideoOnDemand.Entities;
 
 namespace VideoOnDemand.Repositories
---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Repositories/MovieRepository.cs
@@ -0,0 +1,15 @@
+﻿using VideoOnDemand.Data;
+using VideoOnDemand.Entities;
+
+namespace VideoOnDemand.Repositories
+{
+    public class MovieRepository : BaseRepository<Movie>
+    {
+
+        public MovieRepository(VideoOnDemandContext context) : base (context)
+        {
+
+        }
+
+    }
+}
