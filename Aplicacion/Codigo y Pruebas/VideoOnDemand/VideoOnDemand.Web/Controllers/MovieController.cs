882cd3c00ccdbcfea1c38ce9c321d4d8e1c3475c
KevinPool <kevmkc@gmail.com>
2018-08-03 10:17:01 -0500

    movies

---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoOnDemand.Repositories;
+using VideoOnDemand.Web.Helpers;
+using VideoOnDemand.Web.Models;
+
+namespace VideoOnDemand.Web.Controllers
+{
+    public class MovieController : BaseController
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            MovieRepository repository = new MovieRepository(context);
+            var lst = repository.GetAll();
+            var models = MapHelper.Map<IEnumerable<MovieViewModel>>(lst);
+            return View(models);
+        }
+    }
+}
