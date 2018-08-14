6f649a7a24736cd2f60031acf1b9fab744110c05
KevinPool <kevmkc@gmail.com>
2018-08-14 13:15:09 -0500

    a

---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Models/MovieSerieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoOnDemand.Web.Models
+{
+    public class MovieSerieViewModel
+    {
+        public ICollection<MovieViewModel> Movies { get; set; }
+
+        public ICollection<SerieViewModel> Series { get; set; }
+    }
+}
