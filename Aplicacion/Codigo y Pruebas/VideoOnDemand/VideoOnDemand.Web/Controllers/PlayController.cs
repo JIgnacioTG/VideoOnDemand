95bc88b9bd6c3f729091bce0d0e5a644b1065bcd
JIgnacioTG <jitg97@hotmail.com>
2018-08-14 11:09:57 -0500

    Modal

--- Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Controllers/PlayController.cs
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Controllers/PlayController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VideoOnDemand.Repositories;
 
 namespace VideoOnDemand.Web.Controllers
 {
-    public class PlayController : Controller
+    public class PlayController : BaseController
     {
         // GET: Play
         public ActionResult Index()
         {
+            MediaOnPlayRepository mediaonplayRepository = new MediaOnPlayRepository(context);
             return View();
         }
 
---
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Web/Models/MediaOnPlayViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoOnDemand.Web.Models
+{
+    public class MediaOnPlayViewModel
+    {
+        public int Id { get; set; }
+        public long? Milisegundo { get; set; }
+        public int? MediaId { get; set; }
+        public MediaViewModel Media { get; set; }
+        public int? UsuarioId { get; set; }
+        public UsuarioViewModel Usuario { get; set; }
+    }
+}
